1fd62dee1ca3e8ef3949e48d6865bc073f922d22
PyrdePavkki <paavola.pyry@gmail.com>
2021-02-13 17:45:51 +0200

    Added recognition for "CT-"
    Now possible to set a tag and a nickname with "CT-" prefix

--- bot.js
+++ bot.js
@@ -18,6 +18,25 @@ msg.member.setNickname("CT-" + tag[1] + "\"" + name[0] + "\"")
     msg.member.roles.add(verified)
 })
 .catch(msg.reply("Something went wrong. Check the nickname isn't too long"))
+} else if (msg.channel == channel && msg.member.roles.has(unverified) && msg.content.startsWith("CT-")) {
+    let name = msg.content.split(" ")
+    name = name[1]
+    name = name.substr(-1)
+    name = name.substr(1)
+    let tag = msg.content.split(" ")
+    tag = tag[0]
+    tag = tag.split("-")
+    tag = tag[1]
+    tag = tag.toString()
+    if (tag == config.blacklist[0] || tag == config.blacklist[1] || tag == config.blacklist[2] || tag == config.blacklist[3]) {
+    tag = Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10)
+    tag = tag.toString()
+}
+msg.member.setNickname("CT-" + tag[1] + "\"" + name[0] + "\"")
+.then( () => {
+    msg.member.roles.remove(unverified)
+    msg.member.roles.add(verified)
+})
 }
 })
 bot.login(process.env.BOT_TOKEN)
