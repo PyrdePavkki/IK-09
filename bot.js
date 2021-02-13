aea77c461105a634959fe6bd15bc2da77ae0ba16
PyrdePavkki <paavola.pyry@gmail.com>
2021-02-13 19:27:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,17 +2,17 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const blacklist = ["0000", "0001", "6969", "0420"]
 const channel = [process.env.CHANNEL_ID_51ST, process.env.CHANNEL_ID];
-const unverified = {
+let unverified = {
     "797594762668146739": process.env.UNVERIFIED_ROLE_ID_51ST,
     "810169385431203880": process.env.UNVERIFIED_ROLE_ID
 }
-const verified = {
+let verified = {
     "797594762668146739": process.env.VERIFIED_ROLE_ID_51ST,
     "810169385431203880": process.env.VERIFIED_ROLE_ID
 }
 const logs = process.env.LOG_CHANNEL_ID;
 bot.on('message', (msg) => {
-if (channel.includes(msg.channel.id) && unverified.includes(msg.member.roles.cache) && msg.content.startsWith("\"")) { 
+if (channel.includes(msg.channel.id) && unverified.values.includes(msg.member.roles.cache) && msg.content.startsWith("\"")) { 
     logs.send("From: " + msg.guild.id + "Name starting message noticed")
 let name = msg.content.split(" ")
 let tag = msg.author.tag.split("#")
@@ -25,7 +25,7 @@ msg.member.setNickname("CT-" + tag[1] + " " + name[0])
     msg.member.roles.remove(unverified[msg.guild.id.toString()])
     msg.member.roles.add(verified[msg.guild.id.toString()])
 })
-} else if (channel.includes(msg.channel.id) && unverified.includes(msg.member.roles.cache) && msg.content.startsWith("CT-")) {
+} else if (channel.includes(msg.channel.id) && unverified.values.includes(msg.member.roles.cache) && msg.content.startsWith("CT-")) {
     logs.send("From: " + msg.guild.id + "CT- starting message noticed")
     let name = msg.content.split(" ")
     name = name[1]
