efc6283f30295da5703582e7ee8ec19714cd291c
PyrdePavkki <paavola.pyry@gmail.com>
2021-02-13 17:17:39 +0200

    first commit

---
+++ bot.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const config = require('config.json')
+const channel = process.env.CHANNEL_ID;
+const unverified = process.env.UNVERIFIED_ROLE_ID;
+const verified = process.env.VERIFIED_ROLE_ID;
+bot.on('message', (msg) => {
+if (msg.channel == channel && msg.member.roles.has(unverified) && msg.content.startsWith("\"")) { 
+let name = msg.content.split(" ")
+let tag = msg.user.tag.split("#")
+if (tag == config.blacklist[0] || tag == config.blacklist[1] || tag == config.blacklist[2] || tag == config.blacklist[3]) {
+    tag = Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10)
+    tag = tag.toString()
+}
+msg.member.setNickname("CT-" + tag[1] + "\"" + name[0] + "\"")
+.then( () => {
+    msg.member.roles.remove(unverified)
+    msg.member.roles.add(verified)
+})
+.catch(msg.reply("Something went wrong. Check the nickname isn't too long"))
+}
+})
+bot.login(process.env.BOT_TOKEN)
